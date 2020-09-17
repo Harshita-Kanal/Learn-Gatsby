37ea68015d9fde593358632e91aceb8e505b4bdc
Harshita-Kanal <harshita.kgv@gmail.com>
2020-09-17 21:25:31 +0530

    Added Layouts

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p>This is the header</p>
+        </div>
+    )
+}
+
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Layout from "../components/Layout";
 
 const Home = () => {
   return(
+    <Layout>
     <div className="head">
       <h1>Hello, I am Harshita</h1>
     </div>
+    </Layout>
   )
 }
 
